486dd8ac9a4b80dc911178ada4420d99ccd3b0be
houbly <h.blyden@gmail.com>
2018-07-16 15:55:14 +0100

    Added InterUI font, svg icons & header styling

---
+++ src/assets/Icons.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const IconSearch = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+      <path d="M40.4 38l-6.8-6.8c4.2-5.3 3.9-13.1-1-18-5.3-5.3-13.8-5.3-19.1 0-5.3 5.3-5.3 13.8 0 19.1 2.6 2.6 6.1 3.9 9.5 3.9 2.7 0 5.3-.8 7.6-2.4l6.9 6.9 2.9-2.7zm-24.1-8.6c-3.7-3.7-3.7-9.7 0-13.4 1.9-1.9 4.3-2.8 6.7-2.8s4.9.9 6.7 2.8c3.7 3.7 3.7 9.7 0 13.4-3.6 3.7-9.7 3.7-13.4 0z" fill="currentColor"/>
+      <path fill="none" d="M0 0h50v50H0z"/>
+    </svg>
+  )
+}
+
+const IconPlus = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+      <path fill="currentColor" d="M40 23H27V10h-4v13H10v4h13v13h4V27h13z"/>
+      <path fill="none" d="M0 0h50v50H0z"/>
+    </svg>
+  )
+}
+
+export {
+    IconSearch,
+    IconPlus
+}
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,22 +1,61 @@
 import React from "react";
+import {IconPlus, IconSearch } from "../assets/Icons"
+import styled from "styled-components"
+import LogoImg from "../img/Logo.png"
+import AvatarImg from "../img/Avatar.png"
+
+
+const HeaderLogo = styled.div`
+  max-width: 7.5rem;
+  max-height: 5rem;
+  > img{
+    max-width: 100%;
+    max-height: 100%:
+  }
+`
+
+const Avatar = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+  overflow: hidden;
+  border-radius: 50%;
+  > img{
+    max-width: 100%;
+    max-height: 100%:
+  }
+`
+
+const ProfileNav = () => {
+  return (
+    <div className="profile-navigation">
+      <ul>
+        <li>
+          <IconPlus></IconPlus>
+        </li>
+        <li>
+          <IconSearch></IconSearch>
+        </li>
+        <li>
+          <Avatar><img src={AvatarImg} alt=""/></Avatar>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export function MainHeader() {
   return (
     <header>
       <div className="pre-header">
-        <div className="header-logo">Logo Image</div>
-        <div className="profile-navigation"><ul>
-            <a href="" className="profile-img">
-              S
-            </a>
-        </ul></div>
+        <HeaderLogo><img src={LogoImg} alt="Logo"/></HeaderLogo>
+        <ProfileNav></ProfileNav>
       </div>
       <div className="container">
         <div className="page-title">Front Extension 42 Willow Way SE15</div>
         <ol className="breadcrumbs">
           <li>Project Details</li>
           <li className="is-active">Advice</li>
-          <li>Next Steps</li>
+          <li>Next Step</li>
         </ol>
       </div>
     </header>
